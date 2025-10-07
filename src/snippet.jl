aac0579082b9c4ab1471e0cc5777fa23ed86730f
ken-morel <engonken8@gmail.com>
2025-10-07 23:32:31 +0100

    feat(comment): Added support for inline comments on componentcall, snippet def, juliablock, end. But nothing else

--- src/snippet.jl
+++ src/snippet.jl
@@ -13,14 +13,23 @@ for type checking and multiple dispatch.
 struct Snippet{T <: NamedTuple}
     fn::Function
 
-    function Snippet(fn::Function)
+    """
+        Snippet{T}(fn::Function) where T <: NamedTuple
+        Snippet(::Type{T}, fn::Function) where T <: NamedTuple
+        Snippet(fn::Function, ::Type{T}) where T <: NamedTuple
+
+    Creates a snippet with the specified type
+    """
+    function Snippet{T}(fn::Function) where {T <: NamedTuple}
         returns = Base.return_types(fn)
         if length(returns) !== 1 || !(returns[1] <: AbstractVector{<:Component})
             @warn "Snippets should return Vector{<:Component}, but $fn returns $returns"
         end
-        return new(fn)
+        return new{T}(fn)
     end
 end
+Snippet(::Type{T}, fn::Function) where {T <: NamedTuple} = Snippet{T}(fn)
+Snippet(fn::Function, ::Type{T}) where {T <: NamedTuple} = Snippet{T}(fn)
 
 "Call the snippet inner function with the passed arguments"
 (sn::Snippet)(; args...) = sn.fn(; args...)
